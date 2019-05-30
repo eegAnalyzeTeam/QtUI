a452e43a6c99c4e75520c79534a5ba5fce763b22
dongfangyier <1536370121@qq.com>
2019-05-30 21:47:59 +0800

    完成基本界面

--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.uic import loadUi
 
@@ -12,6 +13,25 @@ class MainWindow(QMainWindow):
         self.SelectButton.clicked.connect(self.select_click)
         self.StartButton.clicked.connect(self.start_click)
         self.StopButton.clicked.connect(self.stop_click)
+        self.ImageLabel.setPixmap(QPixmap('icon.jpg'))
+        self.ImageLabel.setScaledContents(True)
+
+        # 样式
+        self.SelectButton.setStyleSheet("QPushButton{color:black}"
+                                        "QPushButton:hover{color:red}"
+                                        "QPushButton{background-color:rgb(78,255,255)}"
+                                        "QPushButton{border:2px}"
+                                        "QPushButton{border-radius:10px}")
+        self.StartButton.setStyleSheet("QPushButton{color:black}"
+                                       "QPushButton:hover{color:red}"
+                                       "QPushButton{background-color:rgb(78,255,255)}"
+                                       "QPushButton{border:2px}"
+                                       "QPushButton{border-radius:10px}")
+        self.StopButton.setStyleSheet("QPushButton{color:black}"
+                                      "QPushButton:hover{color:red}"
+                                      "QPushButton{background-color:rgb(255, 78, 116)}"
+                                      "QPushButton{border:2px}"
+                                      "QPushButton{border-radius:10px}")
 
     def select_click(self):
         fname = QFileDialog.getOpenFileName(self, 'open file', '/')
